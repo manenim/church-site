f50d04a6e8c48e8a9fdffc90cfc4e69cc7a7f2d0
manenim udoh <manenimabasi.udoh@stu.cu.edu.ng>
2023-01-26 11:05:52 +0100

    implemented minor bug fixes

--- components/general/Navbar.tsx
+++ components/general/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import React, { useState } from 'react'
 import { Squash as Hamburger } from 'hamburger-react'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
 
 
 
@@ -15,7 +16,7 @@ const Navbar = () => {
       <nav className='absolute mt-10 w-screen'>
           <div className='mx-auto w-[85%]'>
               <div className='flex h-[4rem] items-center justify-between rounded-t-[32px] bg-white px-8 shadow-2xl'>
-                  <div className="text-[1.5rem] font-bold">LOGO.</div>
+                  <div className="text-[1.5rem] font-bold"><Image src="/assets/img/logo.svg" alt="logo" width={40} height={40} /></div>
                   {/* <Hamburger size={20} toggled={isOpen} toggle={setOpen}/> */}
                   <ul className="hidden items-center justify-between md:flex">
                       <li><Link className={'' + router.asPath == "/" ? 'font-semibold' : ''} href = '/'>Home</Link></li>
